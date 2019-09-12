a541c5f11a4cc5209900b0f282962d197182c83d
Michael Wilson <mewil@umich.edu>
2019-09-11 23:19:47 -0400

    Refactor user repository functions to be cleaner

--- services/user/repository.go
+++ services/user/repository.go
@@ -25,16 +25,15 @@ func NewUserRepository(log logger.Logger, db database.DB) (UserRepository, error
 		log: log.(*zap.SugaredLogger).Named("repository"),
 		db:  db,
 	}
-	if _, err := db.Exec(userSchema); err != nil {
-		return nil, fmt.Errorf("failed to create user table %s", err.Error())
-	}
-	if _, err := db.Exec(followingSchema); err != nil {
-		return nil, fmt.Errorf("failed to create following table %s", err.Error())
+	for _, schema := range []string{userSchema, followingSchema} {
+		if _, err := db.Exec(schema); err != nil {
+			return nil, fmt.Errorf("failed to create table %s", err.Error())
+		}
 	}
 	return &r, nil
 }
 
-const userSchema string = `create table if not exists user (
+const userSchema string = `create table if not exists users (
 	user_id binary(16) not null unique,
 	username varchar(64) collate utf8mb4_bin not null unique,
 	file_id binary(16) not null,
@@ -42,15 +41,15 @@ const userSchema string = `create table if not exists user (
 	description varchar(256) collate utf8mb4_bin,
 	updated_at timestamp default current_timestamp,
 	created_at timestamp default current_timestamp,
-	primary key (email)
+	primary key (user_id)
 ) engine InnoDB default charset utf8mb4 collate utf8mb4_bin;`
 
 const followingSchema string = `create table if not exists following (
 	user_id binary(16) not null unique,
 	following_id binary(16) not null unique,
 	created_at timestamp default current_timestamp,
-	key user(user_id),
-	key user(following_id)
+	key users(user_id),
+	key users(following_id)
 ) engine InnoDB;`
 
 type repository struct {
@@ -60,27 +59,25 @@ type repository struct {
 
 const userPageSize = 25
 
-func (r *repository) CreateUser(userID, username, email string) error {
-	if _, err := r.db.Exec(
-		"insert user set user_id=UUID_TO_BIN(?), username=?, email=?",
+func (r *repository) CreateUser(userID, username, email string) (err error) {
+	_, err = r.db.Exec(
+		"insert users set user_id=UUID_TO_BIN(?), username=?, email=?",
 		userID,
 		username,
 		email,
-	); err != nil {
-		return err
-	}
-	return nil
+	)
+	return
 }
 
 func (r *repository) UserIDExists(userID string) (exists bool, err error) {
-	err = r.db.QueryRow("select exists (select 1 from user where user_id=?)", userID).Scan(
+	err = r.db.QueryRow("select exists (select 1 from users where user_id=?)", userID).Scan(
 		&exists,
 	)
 	return
 }
 
 func (r *repository) UsernameExists(username string) (exists bool, err error) {
-	err = r.db.QueryRow("select exists (select 1 from user where username=?)", username).Scan(
+	err = r.db.QueryRow("select exists (select 1 from users where username=?)", username).Scan(
 		&exists,
 	)
 	return
@@ -89,7 +86,7 @@ func (r *repository) UsernameExists(username string) (exists bool, err error) {
 func (r *repository) GetUser(userID string) (*pb.User, error) {
 	u := pb.User{}
 	if err := r.db.QueryRow(
-		"select BIN_TO_UUID(user_id), name, BIN_TO_UUID(file_id), description, username from user where user_id=UUID_TO_BIN(?) limit 1",
+		"select BIN_TO_UUID(user_id), name, BIN_TO_UUID(file_id), description, username from users where user_id=UUID_TO_BIN(?) limit 1",
 		userID,
 	).Scan(
 		&u.UserID,
@@ -128,31 +125,27 @@ func (r *repository) getFollowingCount(userID string) (n uint32, err error) {
 	return
 }
 
-func (r *repository) FollowUser(userID string, followID string) error {
-	if _, err := r.db.Exec(
+func (r *repository) FollowUser(userID string, followID string) (err error) {
+	_, err = r.db.Exec(
 		"insert following set user_id=UUID_TO_BIN(?), following_id=UUID_TO_BIN(?)",
 		userID,
 		followID,
-	); err != nil {
-		return err
-	}
-	return nil
+	)
+	return
 }
 
-func (r *repository) UnfollowUser(userID string, followID string) error {
-	if _, err := r.db.Exec(
+func (r *repository) UnfollowUser(userID string, followID string) (err error) {
+	_, err = r.db.Exec(
 		"delete from following where user_id=UUID_TO_BIN(?), following_id=UUID_TO_BIN(?) limit 1",
 		userID,
 		followID,
-	); err != nil {
-		return err
-	}
-	return nil
+	)
+	return
 }
 
 const selectFollowersQuery = `
 select BIN_TO_UUID(user.user_id), user.name, BIN_TO_UUID(user.file_id), user.description, user.username
-from user, following
+from users, following
 where following.following_id=UUID_TO_BIN(?) and user.user_id=following.user_id
 order by recorded_at desc
 limit ?,?
@@ -164,7 +157,7 @@ func (r *repository) GetFollowers(userID string, page uint32) ([]*pb.User, error
 
 const selectFollowingQuery = `
 select BIN_TO_UUID(user.user_id), user.name, BIN_TO_UUID(user.file_id), user.description, user.username
-from user, following
+from users, following
 where following.user_id=UUID_TO_BIN(?) and user.user_id=following.user_id
 order by recorded_at desc
 limit ?,?
@@ -180,17 +173,16 @@ func (r *repository) getUserPage(query string, userID string, page uint32) ([]*p
 	if err != nil {
 		return nil, err
 	}
-	results := make([]*pb.User, 0)
+	results := make([]*pb.User, 0, userPageSize)
 	for rows.Next() {
 		u := pb.User{}
-		err := rows.Scan(
+		if err := rows.Scan(
 			&u.UserID,
 			&u.Name,
 			&u.FileID,
 			&u.Description,
 			&u.Username,
-		)
-		if err != nil {
+		); err != nil {
 			return nil, err
 		}
 		results = append(results, &u)

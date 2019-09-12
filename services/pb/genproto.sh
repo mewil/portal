8382cc063f5db9bc9091ac38e0e98e213dfb0e33
Michael Wilson <mewil@umich.edu>
2019-09-12 00:22:48 -0400

    Replace protobuf time fields with time.Time type instead of fixed32

--- services/pb/genproto.sh
+++ services/pb/genproto.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gofast_out=plugins=grpc:. *.proto
+protoc -I=. -I=$GOPATH/src -I=$GOPATH/src/github.com/gogo/protobuf/protobuf --gofast_out=Mgoogle/protobuf/timestamp.proto=github.com/gogo/protobuf/types,plugins=grpc:. *.proto
